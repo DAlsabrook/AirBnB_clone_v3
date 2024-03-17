ab06920a759920a11028805c3de30e2c14e24ba8
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-03-17 03:44:54 +0000

    Update to /state and /stateid to fix dict issue

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -11,16 +11,18 @@ from flask import jsonify, abort, request
 @app_views.route('/states', methods=['GET', 'POST'], strict_slashes=False)
 def states():
     """Get all states"""
+    # Returns all state instances
     if request.method == 'GET':
         states = [state.to_dict() for state in storage.all(State).values()]
         return jsonify(states)
+
+    # Creates a new state instance
     if request.method == "POST":
         http_json = request.get_json(silent=True)
         if http_json is None:
             abort(400, description="Not a JSON")
         if 'name' not in http_json:
             abort(400, description="Missing name")
-
         new_state = State(**http_json)
         new_state.save()
         return jsonify(new_state.to_dict()), 201
@@ -29,15 +31,22 @@ def states():
 @app_views.route('/states/<state_id>', methods=['GET', 'DELETE', 'PUT'])
 def states_with_id(state_id):
     """Get one specific state object by id"""
+    # Check if there is a state with given id
     state = get_state_by_id(state_id)
     if state is None:
         abort(404)
+
+    # Return the state with code 200
     if request.method == 'GET':
-        return state
+        return jsonify(state), 200
+
+    # Delete the state and save with code 200
     elif request.method == "DELETE":
         storage.delete(state)
         storage.save()
         return jsonify({}), 200
+
+    # Update the state with given attributes
     elif request.method == "PUT":
         http_json = request.get_json(silent=True)
         if http_json is None:
@@ -46,12 +55,11 @@ def states_with_id(state_id):
             if key not in ["id", "created_at", "updates_at"]:
                 setattr(state, key, value)
                 state.save()
-        return jsonify(state.to_dict()), 200
+        return jsonify(state), 200
 
 
 def get_state_by_id(state_id):
     """Function to get state by id"""
-    state_obj = [state for state in storage.all(State).values() if state.id == state_id]
-    if not isinstance(state_obj, State):
-        return None
-    return state_obj
+    state_obj = [state.to_dict() for state in storage.all(State).values()
+                 if state.id == state_id]
+    return state_obj[0] if state_obj else None
